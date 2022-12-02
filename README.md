532aaf582f73c012ac7cc6a9bee92724443c76cf
Motwkel-Mohammed <kooli2015@gmail.com>
2022-12-02 20:10:39 +0200

    Done

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Donut-vending-Machine--Dapp
+## Introduction
+
 A Dapp Donut vending Machine project, which have the following functionality:
+
+- Purchase donuts
+- Restock donuts
+- Knowing the number of bought donut for each address
+
+
+
+## Credit
+
+- [Jspruance](https://github.com/jspruance) had an awesome tutorial on [YouTube](https://youtu.be/Qu6GloG0dQk).
+
+  And the link for his repository is right [here](https://github.com/jspruance/block-explorer-tutorials/tree/main/apps/VendingMachine).
+
+## Our  Touch
+
+- Instead of using [infura](https://www.infura.io/) blockchain, we use Ganache blockchain.
+- We add the logic of 'Restock' function in `vending-machine.js` on pages route.
+
+## Member
+
+- [Motwkel Mohammed](https://github.com/Motwkel-Mohammed)
+- Ibrahim Al-Fatih
+- Ahmed Abd-Allah
+
---
+++ frontend/blockchain/vending.js
@@ -0,0 +1,10 @@
+const abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"donutBalances","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getVendingMachineBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"purchase","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"restock","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+
+const vendingMachineContract = web3 => {
+    return new web3.eth.Contract(
+        abi,
+        "0x81aaBB75E14346D28f8B8519F08f9AFd9AdDbed3"
+    )
+}
+
+export default vendingMachineContract
